3ec0265072b7c3de250435eb36d5433f5fde3a4e
Luis Lorenzo Mosquera <luis.lorenzom@udc.es>
2018-04-24 18:47:57 +0200

    Some changes in callMusket native side

--- src/main/native/callMusket.c
+++ src/main/native/callMusket.c
@@ -6,9 +6,34 @@
 
 int main(int argc, char **argv);
 
+char** str_split(char str[], const char delim) {
+  
+  // Some variables declaration
+  char* split = strtok(str, &delim);
+  char *word;
+
+  // Do split
+  while(split != NULL) {
+      printf("%s\n", split);
+      word = split;
+      split=strtok(NULL, &delim);
+  }
+  
+  return 0;
+}
+
 JNIEXPORT void JNICALL Java_es_udc_gac_hmusket_MusketCaller_callMusket(JNIEnv *env, jobject thisObj, jstring jstr) {
+	
+	// Some variables declaration
 	const char *arguments = (*env)->GetStringUTFChars(env, jstr, NULL);
-	printf("%s\n",arguments);
+	char argv[strlen(arguments) + 1];
+	strcpy(argv, args);
+	
+	printf("%s\n",argv);
+	str_split(argv, *" ");
+	
+	// call musket's main
 	main(2,arguments);
+
 	return;
 }
